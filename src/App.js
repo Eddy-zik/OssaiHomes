ff6ea9b888f0cdfd59df7169e2dcaff58d360480
Dayveed021 <boskey021@gmail.com>
2023-03-02 11:26:06 +0100

    OssaiHomes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './grid.scss';
+import {useState} from 'react';
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState('');
+
+  const handleMessageChange = (event) => {
+    // 👇️ access textarea value
+    setMessage(event.target.value);
+    console.log(message);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="message">My Textarea</label>
+      <textarea
+        id="message"
+        name="message"
+        value={message}
+        onChange={handleMessageChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
+// We used the useState hook to store the value of the textarea element in the component's state.
