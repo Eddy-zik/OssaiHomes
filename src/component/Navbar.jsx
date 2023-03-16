8b4bce90f15afdc5b77a517c3b47be4fa60c6f89
Edward Ezekiel-Hart <115160391+Eddy-zik@users.noreply.github.com>
2023-03-17 00:00:29 +0100

    Add files via upload

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { NavLink, Outlet, Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "../styles/Navbar.scss";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons"
 
 export const Navbar = () => {
     const [toggle, setToggle] = useState(false)
--- src/component/Pages/Login.jsx
+++ src/component/Pages/Login.jsx
@@ -1,9 +1,111 @@
 import React from 'react'
 import "../../styles/Login.scss";
+import close from "./close.svg";
+import facebook from "./facebook.svg";
+import google from "./google.svg";
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className='back_login'>
+ <div class="wrapper">
+ <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet"></link>
+        
+        <span class="icon-close">
+        <img src={close} alt="close_icon"
+        height={20} width={20} />
+        </span>
+
+        <div class="form-box login">
+            <h2>Log into your account</h2>
+            <div class="login-register">
+                    <p>Or
+                        <a href="/" class="register-link">Create</a>account</p>
+
+                </div>
+            <form action="#">
+                <div class="input-box">
+                    
+                    <input type="email" required placeholder='Email Address'/> 
+
+                </div>
+                <div class="input-box">
+                    
+                    <input type="password" required placeholder='Password'/>
+                    
+                </div>
+                <div class="remember_me">
+                    <a href="/">forgot password?</a>
+                </div>
+                <button type="submit" className='btn_'>Login</button>
+
+                    <p className='or'>or</p>
+
+                <div>
+                <span className='icons'>
+                <button className='btn_icon' > <img src={google} alt="google_icon" className='img'
+        height={20} width={20}/>Log in with Google</button>
+                </span>
+                
+                <span className='icons'>
+                <button className='btn_icon'><img src={facebook}  className='img'  alt="facebook_icon"
+        height={20} width={20} />Log in with Facebook</button>
+                </span>
+               </div>
+            </form>
+            </div>
+    {/* <div class="form-box register">
+            <h2>Create an account</h2>
+            <div class="register-login">
+                    <p>Or
+                        <a href="/" class="login-link">Login</a>to your account</p>
+
+                </div>
+            <form action="#">
+                <div class="input-box">
+                    
+                    <input type="email" required placeholder='First Name'/> 
+                    <input type="email" required placeholder='Last Name'/> 
+
+                </div>
+                <div class="input-box">
+                    
+                    <input type="email" required placeholder='Email Address'/> 
+
+                </div>
+                <div class="input-box">
+                    
+                    <input type="password" required placeholder='Password'/>
+                    
+                </div>
+                <div class="input-box">
+                    
+                    <input type="password" required placeholder='Confirm Password'/>
+                    
+                </div>
+                <div class="T&C">
+                    <label for="box">
+                        <input type="checkbox"/>agree to the terms &
+                    </label>
+                    <a href="#">conditions</a><br/>
+                <button type="submit" className='btn'>Sign up</button>
+                </div>
+                    <p className='or'>OR</p>
+
+                <div>
+                <span className='icons'>
+                <button> <img src={google} alt="google_icon"
+        height={20} width={20}/> Log in with Google</button>
+                </span>
+                
+                <span className='icons'>
+                <button><img src={facebook} alt="facebook_icon"
+        height={20} width={20} />Log in with Facebook</button>
+                </span>
+               </div>
+            </form>
+            </div> */}
+</div>
+</div>
     )
 }
 
