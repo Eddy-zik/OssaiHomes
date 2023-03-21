52ea52754c568f96767053cbc80148cf18e17eb5
Edward Ezekiel-Hart <115160391+Eddy-zik@users.noreply.github.com>
2023-03-21 11:55:34 +0100

    Updated_loginPage

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react"
+import React, { useState } from 'react'
 import { NavLink, Outlet, Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "../styles/Navbar.scss";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Login.scss";
+import close from "./Pages/close.svg";
+import facebook from "./Pages/facebook.svg";
+import google from "./Pages/google.svg";
 
 export const Navbar = () => {
     const [toggle, setToggle] = useState(false)
@@ -10,6 +14,15 @@ export const Navbar = () => {
         console.log("clicked")
         setToggle(!toggle)
     }
+    const[popup, setPop] = useState(false);
+ const openPopup=()=>{
+    setPop(!popup);
+ }
+
+    const closePopup=()=>{
+        setPop(false)
+    }
+ 
     return (
         <>
             <div className="nav">
@@ -25,15 +38,121 @@ export const Navbar = () => {
                     </ul>
                     <ul>
                         <li><NavLink to="/myagent" className={({ isActive }) => (isActive ? 'active' : 'nav-list')}>MyAgent</NavLink></li>
-                        <NavLink to="/login">
-                            <button className="btn">
+                      
+                            <button className="btn" onClick={openPopup}>
                                 Login
                             </button>
-                        </NavLink>
+                     
                     </ul>
                 </nav>
             </div>
             <Outlet />
+
+
+            {popup?
+        <div className='back_login'>
+            <div className="wrapper">
+                <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet"></link>
+
+                <span class="icon-close" onClick={closePopup}>
+                    <img src={close} alt="close_icon"
+                        height={20} width={20} />
+                </span>
+
+                <div class="form-box login">
+                    <h2>Log into your account</h2>
+                    <div class="login-register">
+                        <p>Or
+                            <span class="register-link">Create</span>account</p>
+
+                    </div>
+                    <form action="#">
+                        <div class="input-box">
+
+                            <input type="email" required placeholder='Email Address' />
+
+                        </div>
+                        <div class="input-box">
+
+                            <input type="password" required placeholder='Password' />
+
+                        </div>
+                        <div class="remember_me">
+                            <a href="/">forgot password?</a>
+                        </div>
+                        <button type="submit" className='btn_'>Login</button>
+
+                        <p className='or'>or</p>
+
+                        <div>
+                            <span className='icons'>
+                                <button className='btn_icon' > <img src={google} alt="google_icon" className='img'
+                                    height={20} width={20} />Log in with Google</button>
+                            </span>
+
+                            <span className='icons'>
+                                <button className='btn_icon'><img src={facebook} className='img' alt="facebook_icon"
+                                    height={20} width={20} />Log in with Facebook</button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+                {/* <div class="form-box register">
+            <h2>Create an account</h2>
+            <div class="register-login">
+                    <p>Or
+                        <span class="login-link">Login</span>to your account</p>
+
+                </div>
+            <form action="#">
+                <div class="input-box_register">
+                    
+                    <input type="email" required placeholder='First Name'/> 
+                    <input type="email" required placeholder='Last Name'/> 
+
+                </div>
+                <div class="input-box">
+                    
+                    <input type="email" required placeholder='Email Address'/> 
+
+                </div>
+                <div class="input-box">
+                    
+                    <input type="password" required placeholder='Password'/>
+                    
+                </div>
+                <div class="input-box">
+                    
+                    <input type="password" required placeholder='Confirm Password'/>
+                    
+                </div>
+                <div class="t_c">
+                    <label for="box">
+                        <input type="checkbox" required />agree to the
+                    </label>
+                    <a href="#">terms & conditions</a><br/>
+                <button type="submit" className='btn_'>Sign up</button>
+                </div>
+                <p className='or'>or</p>
+
+<div>
+<span className='icons'>
+<button className='btn_icon' > <img src={google} alt="google_icon" className='img'
+height={20} width={20}/>Log in with Google</button>
+</span>
+
+<span className='icons'>
+<button className='btn_icon'><img src={facebook}  className='img'  alt="facebook_icon"
+height={20} width={20} />Log in with Facebook</button>
+</span>
+</div>
+            </form>
+            </div> */}
+
+            </div>
+        </div>:""}
+
+
         </>
     )
 }
--- src/component/Pages/Login.jsx
+++ src/component/Pages/Login.jsx
@@ -1,112 +1,117 @@
-import React from 'react'
-import "../../styles/Login.scss";
-import close from "./close.svg";
-import facebook from "./facebook.svg";
-import google from "./google.svg";
+// import React, { useState } from 'react'
+// import "../../styles/Login.scss";
+// import close from "./close.svg";
+// import facebook from "./facebook.svg";
+// import google from "./google.svg";
+// // import { useState } from "react";
 
-const Login = () => {
-    return (
-        <div className='back_login'>
- <div class="wrapper">
- <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet"></link>
-        
-        <span class="icon-close">
-        <img src={close} alt="close_icon"
-        height={20} width={20} />
-        </span>
+// const Login = () => {
 
-        <div class="form-box login">
-            <h2>Log into your account</h2>
-            <div class="login-register">
-                    <p>Or
-                        <a href="/" class="register-link">Create</a>account</p>
+ 
+//     return (
+    
+//         <div className='back_login'>
+//             <div className="wrapper">
+//                 <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet"></link>
 
-                </div>
-            <form action="#">
-                <div class="input-box">
+//                 <span class="icon-close">
+//                     <img src={close} alt="close_icon"
+//                         height={20} width={20} />
+//                 </span>
+
+//                 <div class="form-box login">
+//                     <h2>Log into your account</h2>
+//                     <div class="login-register">
+//                         <p>Or
+//                             <a class="register-link">Create</a>account</p>
+
+//                     </div>
+//                     <form action="#">
+//                         <div class="input-box">
+
+//                             <input type="email" required placeholder='Email Address' />
+
+//                         </div>
+//                         <div class="input-box">
+
+//                             <input type="password" required placeholder='Password' />
+
+//                         </div>
+//                         <div class="remember_me">
+//                             <a href="/">forgot password?</a>
+//                         </div>
+//                         <button type="submit" className='btn_'>Login</button>
+
+//                         <p className='or'>or</p>
+
+//                         <div>
+//                             <span className='icons'>
+//                                 <button className='btn_icon' > <img src={google} alt="google_icon" className='img'
+//                                     height={20} width={20} />Log in with Google</button>
+//                             </span>
+
+//                             <span className='icons'>
+//                                 <button className='btn_icon'><img src={facebook} className='img' alt="facebook_icon"
+//                                     height={20} width={20} />Log in with Facebook</button>
+//                             </span>
+//                         </div>
+//                     </form>
+//                 </div>
+//                 <div class="form-box register">
+//             <h2>Create an account</h2>
+//             <div class="register-login">
+//                     <p>Or
+//                         <a class="login-link">Login</a>to your account</p>
+
+//                 </div>
+//             <form action="#">
+//                 <div class="input-box_register">
                     
-                    <input type="email" required placeholder='Email Address'/> 
+//                     <input type="email" required placeholder='First Name'/> 
+//                     <input type="email" required placeholder='Last Name'/> 
 
-                </div>
-                <div class="input-box">
+//                 </div>
+//                 <div class="input-box">
                     
-                    <input type="password" required placeholder='Password'/>
-                    
-                </div>
-                <div class="remember_me">
-                    <a href="/">forgot password?</a>
-                </div>
-                <button type="submit" className='btn_'>Login</button>
+//                     <input type="email" required placeholder='Email Address'/> 
 
-                    <p className='or'>or</p>
-
-                <div>
-                <span className='icons'>
-                <button className='btn_icon' > <img src={google} alt="google_icon" className='img'
-        height={20} width={20}/>Log in with Google</button>
-                </span>
-                
-                <span className='icons'>
-                <button className='btn_icon'><img src={facebook}  className='img'  alt="facebook_icon"
-        height={20} width={20} />Log in with Facebook</button>
-                </span>
-               </div>
-            </form>
-            </div>
-    {/* <div class="form-box register">
-            <h2>Create an account</h2>
-            <div class="register-login">
-                    <p>Or
-                        <a href="/" class="login-link">Login</a>to your account</p>
-
-                </div>
-            <form action="#">
-                <div class="input-box">
+//                 </div>
+//                 <div class="input-box">
                     
-                    <input type="email" required placeholder='First Name'/> 
-                    <input type="email" required placeholder='Last Name'/> 
+//                     <input type="password" required placeholder='Password'/>
+                    
+//                 </div>
+//                 <div class="input-box">
+                    
+//                     <input type="password" required placeholder='Confirm Password'/>
+                    
+//                 </div>
+//                 <div class="t_c">
+//                     <label for="box">
+//                         <input type="checkbox" required />agree to the
+//                     </label>
+//                     <a href="#">terms & conditions</a><br/>
+//                 <button type="submit" className='btn_'>Sign up</button>
+//                 </div>
+//                 <p className='or'>or</p>
 
-                </div>
-                <div class="input-box">
-                    
-                    <input type="email" required placeholder='Email Address'/> 
+// <div>
+// <span className='icons'>
+// <button className='btn_icon' > <img src={google} alt="google_icon" className='img'
+// height={20} width={20}/>Log in with Google</button>
+// </span>
 
-                </div>
-                <div class="input-box">
-                    
-                    <input type="password" required placeholder='Password'/>
-                    
-                </div>
-                <div class="input-box">
-                    
-                    <input type="password" required placeholder='Confirm Password'/>
-                    
-                </div>
-                <div class="T&C">
-                    <label for="box">
-                        <input type="checkbox"/>agree to the terms &
-                    </label>
-                    <a href="#">conditions</a><br/>
-                <button type="submit" className='btn'>Sign up</button>
-                </div>
-                    <p className='or'>OR</p>
+// <span className='icons'>
+// <button className='btn_icon'><img src={facebook}  className='img'  alt="facebook_icon"
+// height={20} width={20} />Log in with Facebook</button>
+// </span>
+// </div>
+//             </form>
+//             </div>
 
-                <div>
-                <span className='icons'>
-                <button> <img src={google} alt="google_icon"
-        height={20} width={20}/> Log in with Google</button>
-                </span>
-                
-                <span className='icons'>
-                <button><img src={facebook} alt="facebook_icon"
-        height={20} width={20} />Log in with Facebook</button>
-                </span>
-               </div>
-            </form>
-            </div> */}
-</div>
-</div>
-    )
-}
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Login
+// export default Login
